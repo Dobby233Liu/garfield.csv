2c29cf672e8368c8162989c75c9ca329daf9543c
Liu Wenyuan <15816141883@163.com>
2021-04-30 09:07:17 -0500

    Then fix it

--- main.py
+++ main.py
@@ -2,85 +2,93 @@ import re
 import csv
 
 def stripm(text):
+  
   arr = text.splitlines()
-  _ = ""
+  ret = ""
   for i in arr:
-    __ = i.strip()
-    _ = _ + __ + "\n"
+    
+    ret = ret + i.strip() + "\n"
+    
   _ = _.strip()
+  
   return _
 
+# TODO: account for dlibert
 def _find_first_comicid(line, ln=None):
+  
   try:
+    
+    # QUIRK: dataset has a spew of typos, so the regex has to be complex
     result = re.findall(r'^(\s|)((g[as]\w+)(\s|)(--|\.\.))', line)
     if len(result) <= 0:
-      raise IndexError("no match for regex", result, line)
+      raise IndexError("No match for regex", result, line)
     if len(result[0]) <= 0:
-      raise IndexError("no group in match 0 (???)", result[0], line)
+      raise IndexError("No group in match 0 in regex (undefined behaviour)", result[0], line)
 
     return result[0][1:]
+  
   except Exception as e:
-    raise IndexError("can't find comicid for line (line number %s)" % ln) from e
+    
+    raise IndexError("Can't find comicid for line number %s" % ln) from e
 
 def cleanup(input_file, output):
-  lines = input_file.readlines()
+  
+  lines = input_file.readlines() # WARNING: this is bad, though this is fine yet
   writer = csv.writer(output)
 
   _skip_ahead = 0
 
   for i in range(len(lines)):
-    # Skip-ahead
+    
+    # skip-ahead
     if _skip_ahead >= 1:
       _skip_ahead = _skip_ahead - 1
       continue
     
-    # find comicid (for merging megahack)
+    # find comicid (for merging lines together)
     comicid = _find_first_comicid(lines[i], ln=i)
     
     _proc_line = ""
-    # megahack to merge lines to one
+    
+    # hack to merge lines to one
     for i2 in range(len(lines) - i):
+      
       _loop_line = lines[i + i2].strip()
       _sub_comicid = []
+      
       try:
         _sub_comicid = _find_first_comicid(_loop_line, ln=i + i2)
-      except: # line has no comicid header
+      except: # line has no comicid header? TODO: that even happens???
         _proc_line += " " + _loop_line
         _skip_ahead += 1
         continue
       
       if _sub_comicid[1] != comicid[1]:
         break
-
-      #if comicid[1] == "ga210214":
-      #  print(_proc_line)
-      #  print(_loop_line)     
-      if i2 == 0:
+   
+      if i2 == 0: # NTS: why did i do this?
         _proc_line += "-"
       _proc_line += _loop_line[len(comicid[0]):]
-      if i2 != 0:
+      
+      if i2 > 0: # NTS: not skipped one line self
         _skip_ahead += 1
     
-    # Final processing
-    #if "catburger" in _proc_line or comicid[1] == "ga210214":
-    #  print(_proc_line)
+    # final processing
+    # TODO: what if i commented this out?
     _proc_line = re.sub("(\s)+", r"\1", _proc_line)
-    #_proc_line = re.sub("- -", "-  -", _proc_line)
     _proc_line = "\n- ".join(_proc_line.split("- "))
     _proc_line = stripm(_proc_line)
-    #if "catburger" in _proc_line or comicid[1] == "ga210214":
-    #  print(_proc_line)
 
-    writer.writerow([_proc_line, comicid[1]])
+    writer.writerow([_proc_line, comicid[1]]) # NOTE: this accounts for gpt-2-simple, which reads [0] only for csvs
+    
   return
   
-output_to_file = "ga.csv"
-w = open(output_to_file, "w", encoding="utf-8", newline='')
-
-with open(old_file_name, 'r', encoding="windows-1252") as f:
-  try:
-    cleanup(f, w)
-  finally:
-    w.flush()
-    w.close()
-file_name = output_to_file
+if __name__ == "__main__":
+  # TODO: write a actual cli
+  import sys
+  fn = sys.argv[2]
+  datname = sys.argv[1]
+  with sys.stdout if fn == "-" else open(fn, "w", encoding="utf-8", newline='') as w:
+    # QUIRK: why the fuck it has this encoding???
+    with sys.stdin if datname == "-" else open(datname, 'r', encoding="windows-1252") as f:
+      cleanup(f, w)
