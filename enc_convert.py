d3edf679b38e40259f740305a7716e338260680e
Liu Wenyuan <15816141883@163.com>
2021-05-01 05:41:56 -0500

    [skip ci] de tchar

--- enc_convert.py
+++ enc_convert.py
@@ -9,7 +9,25 @@ Convert with this tool before throwing it to main.py
 
 import sys
 
+def input_enc(args=sys.argv[1:]):
+  if args[2] != "detect":
+    return args[2]
+  from chardet.universaldetector import UniversalDetector
+  ret = {"lord": "latin-1"} # LORD latin-1
+  detector = UniversalDetector()
+  with open(argv[0], "r", encoding="latin-1") as f: # encoding="ascii", errors="surrogateescape"
+    for line in f.readlines():
+      detector.feed(line)
+      if detector.done:
+        break
+  detector.close()
+  ret = ret.result
+  print(ret)
+  ret = ret["encoding"]
+  return ret
+
+enc = input_enc()
 with open(sys.argv[1], "rb") as f:
   with open(sys.argv[2], "wb") as w:
     while (b := f.read(1)):
-        w.write(b.decode(sys.argv[3]).encode(sys.argv[4]))
+        w.write(b.decode(enc).encode(sys.argv[4]))
