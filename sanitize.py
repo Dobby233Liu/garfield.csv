7b562e1a61ba01ae33c56371a9c8bcbfc059891b
Liu Wenyuan <15816141883@163.com>
2021-05-22 21:52:30 -0500

    contiune

--- sanitize.py
+++ sanitize.py
@@ -6,7 +6,11 @@ with open(sys.argv[1]) as f:
     c = csv.reader(f)
     next(c) # discard header
     for r in c:
-      magic = r[1]
-      if ids.count(magic) > 0:
-        raise IndexError("comic '%s' already exists in csv" % magic)
-      ids.append(magic)
+      try:
+        magic = r[1]
+        if ids.count(magic) > 0:
+            raise IndexError("comic '%s' already exists in csv" % magic)
+        ids.append(magic)
+      except IndexError as e:
+        print(e)
+        print("-"*20)
