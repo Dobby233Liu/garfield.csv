259f7bfa17281ba6467d996f52fa18373fe7e423
Liu Wenyuan <15816141883@163.com>
2021-07-29 15:53:52 +0800

    [skip ci]

---
+++ quirk_fixer.py
@@ -0,0 +1,7 @@
+unlabeled_entries = {
+    "Why did you add this button the the user interface? You told me to. - Why would I tell you that` You always suggest random changes to create the illusion of adding value. - Well, remove that button. It's only on your copy.": ("070202", "", "", "")
+}
+
+def find_first_comicid_quirkfix(text):
+    fix = getattr(text, unlabeled_entries, None)
+    return fix
