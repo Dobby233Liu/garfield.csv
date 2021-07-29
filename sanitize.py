30e596ac60143f6ea4186ced1803fe230f6d1488
Liu Wenyuan <15816141883@163.com>
2021-07-29 21:08:17 +0800

    ok

--- sanitize.py
+++ sanitize.py
@@ -9,13 +9,13 @@ with open(sys.argv[1]) as f:
     for r in c:
       try:
         magic = r[1]
+        if tsrts.get(magic, None) == None:
+            tsrts[magic] = []
+        tsrts[magic].append(r[0])
         if ids.count(magic) > 0:
             print(tsrts[magic])
             raise IndexError("comic '%s' already exists in csv" % magic)
         ids.append(magic)
-        if tsrts.get(magic, None) == None:
-            tsrts[magic] = []
-        tsrts[magic].append(r)
       except IndexError as e:
         print(e)
         print("-"*20)
