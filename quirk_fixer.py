91cad34c0358610c85565e4d1cce8be41698154d
Liu Wenyuan <15816141883@163.com>
2021-07-29 20:46:52 +0800

    [skip ci]

--- quirk_fixer.py
+++ quirk_fixer.py
@@ -3,8 +3,9 @@ Fills the blank of the automagic with human fixups.
 """
 
 # Lookup table to fix comicids
+# line: (...comicid regex params, actual clip length)
 comicids_fix = {
-    "Why did you add this button the the user interface? You told me to. - Why would I tell you that` You always suggest random changes to create the illusion of adding value. - Well, remove that button. It's only on your copy.": ("070202", "", "", "")
+    "Why did you add this button the the user interface? You told me to. - Why would I tell you that` You always suggest random changes to create the illusion of adding value. - Well, remove that button. It's only on your copy.": ("070202", "", "", "", 0)
 }
 
 def find_first_comicid_quirkfix(text):
