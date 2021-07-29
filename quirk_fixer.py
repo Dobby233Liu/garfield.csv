db8c7541d9a30cd8e8461f332f7f2efa3262377e
Liu Wenyuan <15816141883@163.com>
2021-07-29 16:01:25 +0800

    Update quirk_fixer.py

--- quirk_fixer.py
+++ quirk_fixer.py
@@ -1,12 +1,12 @@
 """
-Defacto patching to the datasets.
-Fills the blank of automatic stuff with human fixups.
+Fills the blank of the automagic with human fixups.
 """
 
-unlabeled_entries = {
+# Lookup table to fix comicids
+comicids_fix = {
     "Why did you add this button the the user interface? You told me to. - Why would I tell you that` You always suggest random changes to create the illusion of adding value. - Well, remove that button. It's only on your copy.": ("070202", "", "", "")
 }
 
 def find_first_comicid_quirkfix(text):
-  fix = getattr(text, unlabeled_entries, None)
+  fix = getattr(comicids_fix, text, None)
   return fix
