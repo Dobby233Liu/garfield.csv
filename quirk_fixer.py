148e07efb35e5aa411430d5cb57f7cac71504f97
Liu Wenyuan <15816141883@163.com>
2021-07-29 21:49:06 +0800

    Update quirk_fixer.py

--- quirk_fixer.py
+++ quirk_fixer.py
@@ -6,7 +6,7 @@ Fills the blank of the automagic with human fixups.
 # line: (...comicid regex params, actual clip length)
 comicids_fix = {
     # Dilbert
-    "Why did you add this button the the user interface? You told me to. - Why would I tell you that` You always suggest random changes to create the illusion of adding value. - Well, remove that button. It's only on your copy.": ("070202", "", "", "", 0)
+    "Why did you add this button the the user interface? You told me to. - Why would I tell you that` You always suggest random changes to create the illusion of adding value. - Well, remove that button. It's only on your copy.": ("070202", "", "", "", 0),
     "- Your performance was excellent, but there's no bonus this year - Why not? - The company lost a fortune in the Elbonian currency collapse. - But in a way, it's your own fault for working here. - Thanks. - That takes the sting out.": ("980826", "", " ", "--", -1),
     "- I must mark my territory by insisting on a change to the prototype. - Give it a wireless Internet option. It already has one. - What doesn't it have? An idiot designing it.": ("050113", "", " ", "--", -1),
     # TODO
