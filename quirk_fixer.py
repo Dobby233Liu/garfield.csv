248afd643ecec40d88e78229a10fa96abac2bc07
Liu Wenyuan <15816141883@163.com>
2021-07-29 15:57:15 +0800

    [skip ci]

--- quirk_fixer.py
+++ quirk_fixer.py
@@ -1,3 +1,8 @@
+"""
+Defacto patching to the datasets.
+Fills the blank of automatic stuff with human fixups.
+"""
+
 unlabeled_entries = {
     "Why did you add this button the the user interface? You told me to. - Why would I tell you that` You always suggest random changes to create the illusion of adding value. - Well, remove that button. It's only on your copy.": ("070202", "", "", "")
 }
