32385f5ab9441fed7300bc3a40587baed7ea6416
Liu Wenyuan <15816141883@163.com>
2021-07-30 12:03:25 +0800

    [skip ci] check more

--- sanitize.py
+++ sanitize.py
@@ -1,21 +1,40 @@
-import csv, sys
+import csv, sys, traceback
 
 ids = []
 tsrts = {}
+bad = False
+reject = False
+num = 0
+
+def eprint(*args, **kwargs):
+  print(*args, **kwargs, file=sys.stderr)
 
 with open(sys.argv[1]) as f:
+  try:
     c = csv.reader(f)
-    next(c) # discard header
-    for r in c:
-      try:
-        magic = r[1]
-        if tsrts.get(magic, None) == None:
-            tsrts[magic] = []
-        tsrts[magic].append(r[0])
-        if ids.count(magic) > 0:
-            print(tsrts[magic])
-            raise IndexError("comic '%s' already exists in csv" % magic)
-        ids.append(magic)
-      except IndexError as e:
-        print(e)
-        print("-"*20)
+  except:
+    bad = True
+    traceback.print_exc()
+    pass
+  next(c) # discard header
+  for r in c:
+    num += 1
+    tbad = False
+    magic = r[1].strip()
+    if not magic:
+      tbad = bad = True
+      eprint("strip #%d has falsy magic: '%s'" % (num, r[1]))
+    if tsrts.get(magic, None) == None:
+      tsrts[magic] = []
+    tsrts[magic].append(r[0])
+    if ids.count(magic) > 0:
+      print(tsrts[magic])
+      tbad = bad = True
+      eprint("strip '%s' already exists in csv" % magic)
+    ids.append(magic)
+    if tbad:
+      eprint("-"*20)
+
+if bad and reject:
+  print("This CSV file has failed the check.")
+  sys.exit(1)
