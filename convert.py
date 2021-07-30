7a3b6c444a5fc268510f9e2a03d2be00bcff7d0e
Liu Wenyuan <15816141883@163.com>
2021-07-30 18:40:44 +0800

    Update convert.py

--- convert.py
+++ convert.py
@@ -108,7 +108,8 @@ def cleanup(input_file, output):
 
         # postprocessing
         def splitline(text):
-            text = re.sub(r"((\s+|^)-+(\s+|[^\S]+|))", "\n", text, flags=re.I).strip()
+            # Lazy version - revert if problematic
+            text = re.sub(r"(\s+|^|)-(\s+|[^\S]+|)", "\n", text, flags=re.I).strip()
             arr = text.splitlines()
             text = "\n".join(list(map(lambda x: "- " + x.strip(), arr)))
             return text.strip()
